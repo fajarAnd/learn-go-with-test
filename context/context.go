6e1b6a5a7d94f4c9486ba82ca13bd07c18648baf
fajarandriansyah <fajarandriansyah1988@gmail.com>
2020-12-06 22:06:05 +0700

    using spystore

--- context/context.go
+++ context/context.go
@@ -6,11 +6,25 @@ import (
 )
 
 func Server(store Store) http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, store.Fetch())
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }
 
 type Store interface {
 	Fetch() string
+	Cancel()
 }
