a2acb05a9f2c23d5d95d9dacf419adb8f526e980
fajar andriansyah <fajarandriansyah1988@gmail.com>
2021-01-19 15:14:43 +0700

    Learn select (#4)
    * select v 1
    
    * select

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -1,25 +1,50 @@
 package concurrency
 
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
 type WebsiteChecker func(string) bool
 type result struct {
 	string
 	bool
+	time.Time
 }
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
-	results := make(map[string]bool)
+	results := make(map[string]bool, len(urls))
 	resultChannel := make(chan result)
 
-	for _, url := range urls {
-		go func(u string) {
-			resultChannel <- result{u, wc(u)}
-		}(url)
+	fmt.Println("Goroutine 1: ", runtime.NumGoroutine())
+	//for _, url := range urls {
+	//	go func(u string) {
+	//		resultChannel <- result{u, wc(u)}
+	//	}(url)
+	//}
+
+	go func() {
+		for _, url := range urls {
+			resultChannel <- result{url, wc(url), time.Now()}
+		}
+		close(resultChannel)
+	}()
+
+	fmt.Println("Goroutine 2: ", runtime.NumGoroutine())
+
+	//for i := 0; i < len(urls); i++ {
+	//	result := <-resultChannel
+	//	results[result.string] = result.bool
+	//}
+
+	for v := range resultChannel {
+		results[v.string] = v.bool
+		fmt.Println("inside range: ", v.Time)
 	}
 
-	for i := 0; i < len(urls); i++ {
-		result := <-resultChannel
-		results[result.string] = result.bool
-	}
+	fmt.Println("Goroutine 3: ", runtime.NumGoroutine())
+
 
 	return results
 }
